91dbe141cfc2d6ed4cf8eed6730f4f46e17c5114
alessiozaccato <alessiozaccato@gmail.com>
2025-02-07 12:41:30 +0100

    ADD:bonus section

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hello World")
+//base exercise
+// console.log("Hello World");
+
+//bonus section 
+const args = process.argv.slice(2);
+const message = args[0] || "Hello World";
+
+console.log(message);
+
